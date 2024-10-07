6f4119682fac7369d1c80fddf05b37e042413d76
Javier Icuté <pablo.icute97@gmail.com>
2024-10-06 18:24:58 -0600

    Este es un proyecto comenzado desde GitHub

---
+++ interactividad.js
@@ -0,0 +1,4 @@
+console.log("Hola de nuevo Javier!")
+const primeraVariable = document.querySelector('#primerParrafo')
+
+primeraVariable.innerHTML = "Hola de nuevo Javier, gracias por no rendirte porque aún si el camino no esta resultadon como lo esperabas aún estas aquí, mañana te irá muy bien ya verás y si no es así no te preocupes tendrá solución."
